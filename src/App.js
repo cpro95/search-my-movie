15c6b01987ed6fab4c98f6d8fcdf2f901782e4e3
Manki Kim <cpro95@gmail.com>
2019-05-19 08:57:37 +0900

    Add Bulma CSS, fix README.md, rm logo.svg

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="section">
+      <div className="hero is-info">
+        <div className="hero-body">
+          <h1 className="title">Hello World</h1>
+        </div>
+      </div>
+    </section>
   );
 }
 
